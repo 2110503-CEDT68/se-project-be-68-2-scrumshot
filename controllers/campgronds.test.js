26f43cf9f7b2aa9096bb03a49a64949a8c3c3c62
theme222 <sira.tongsima@yahoo.com>
2026-04-27 13:29:43 +0700

    finished campground deletion unit test

--- controllers/campgronds.test.js
+++ controllers/campgronds.test.js
@@ -8,9 +8,12 @@ jest.mock('../models/Booking');
 
 describe("Campground Deletion Test", () => {
     
-    const sampleId = "abcdefghijklmnopqrstuvwxyz_id";
-    const sampleCampground = { _id: sampleId }
-    const req = { params: { id: sampleId } }
+    const sampleCampgroundId = "campground_id";
+    const sampleBookingId = "booking_id";
+    const sampleCampground = { _id: sampleCampgroundId }
+    const sampleBooking = { _id: sampleBookingId, bookDate: (new Date() - 1), bookEndDate: (new Date() + 1), }
+    
+    const req = { params: { id: sampleCampgroundId } }
     const res = { status: jest.fn().mockReturnThis(), json: jest.fn() }
     const next = jest.fn();
     
@@ -25,17 +28,46 @@ describe("Campground Deletion Test", () => {
         
         await campgrounds.deleteCampground(req, res, next)
         
-        expect(Campground.findById).toHaveBeenCalledWith(sampleId);
-        expect(Campground.deleteOne).toHaveBeenCalledWith({ _id: sampleId });
+        expect(Campground.findById).toHaveBeenCalledWith(sampleCampgroundId);
+        expect(Booking.find).toHaveBeenCalled(); // it calls with new Date() which is impossible to do a check.
+        expect(Campground.deleteOne).toHaveBeenCalledWith({ _id: sampleCampgroundId });
         expect(res.status).toHaveBeenCalledWith(200);
         expect(res.json).toHaveBeenCalledWith({ success: true, data: {} });
     });
     
     it("should prevent campground deletion with valid bookings and return status 400", async () => {
+        Campground.findById.mockResolvedValue(sampleCampground);
+        Booking.find.mockResolvedValue([sampleBooking, sampleBooking]);
         
+        await campgrounds.deleteCampground(req, res, next)
+        
+        expect(Campground.findById).toHaveBeenCalledWith(sampleCampgroundId);
+        expect(Booking.find).toHaveBeenCalled();
+        expect(Campground.deleteOne).not.toHaveBeenCalled();
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({ success: false, message: "Cannot delete campground with 2 active booking(s). Please cancel all active bookings first." });
     });
     
     it("should return a 404 status if campground not found", async () => {
+        Campground.findById.mockResolvedValue(null);
         
+        await campgrounds.deleteCampground(req, res, next)
+        
+        expect(Campground.findById).toHaveBeenCalledWith(sampleCampgroundId);
+        expect(Campground.deleteOne).not.toHaveBeenCalled();
+        expect(res.status).toHaveBeenCalledWith(404);
+        expect(res.json).toHaveBeenCalledWith({ success: false, message: "Campground not found with id of " + sampleCampgroundId });
     });
+   
+    it("should handle error by returning a 400 status", async () => {
+        Campground.findById.mockResolvedValue(sampleCampground);
+        Booking.find.mockResolvedValue([]);
+        
+        const req = { }
+        await campgrounds.deleteCampground(req, res, next)
+        
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalled();
+    });
+    
 })
